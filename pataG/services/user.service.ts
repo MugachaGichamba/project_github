59b32cb4babfcc9f74a2c5ced5a1356370a97eca
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-01 05:20:44 +0300

    Imported httpclient from angular/common

--- pataG/services/user.service.ts
+++ pataG/services/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   getUsers(){
     return [{
