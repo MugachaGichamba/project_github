d3bb53e6c5d54d0d598f0a71dc01d1bbbc5a10bf
Mugacha Gichamba <mugachadjango@gmail.com>
2019-04-01 04:55:37 +0300

    service successfully renders hardcoded array data

--- pataG/services/user.service.ts
+++ pataG/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor() { }
+
+  getUsers(){
+    return [{
+      "id" : 1, "name" : "gichamba", "age" : 30
+    }]
+  }
 }
